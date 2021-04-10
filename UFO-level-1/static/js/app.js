c57b606f7f4079389edc52d38471479ce603a0d2
Steven Gaetz <gaetz025@umn.edu>
2021-04-09 19:21:02 -0500

    got data to load

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
-
+console.log("app.js loaded");
+console.log(data);
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach((siting) => {
+    var row = tbody.append("tr");
+    Object.entries(siting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
