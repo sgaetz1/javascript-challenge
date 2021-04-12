d944b5998a4bc0e6693bafa153973e38fe254c45
Steven Gaetz <gaetz025@umn.edu>
2021-04-11 20:49:51 -0500

    worked on form control

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,14 +2,32 @@
 var tableData = data;
 console.log("app.js loaded");
 console.log(data);
+
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
 data.forEach((siting) => {
     var row = tbody.append("tr");
+
     Object.entries(siting).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
-  });
+
+});
   
+
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+  d3.event.preventDefault();
+
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  
+};
