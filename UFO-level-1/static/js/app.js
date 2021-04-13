2b95a55f15d3f7484fdab1c73287beed1ce50df8
Steven Gaetz <gaetz025@umn.edu>
2021-04-12 23:06:15 -0500

    got filter to work

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,16 +6,16 @@ console.log(data);
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-data.forEach((siting) => {
+tableData.forEach((siting) => {
     var row = tbody.append("tr");
 
     Object.entries(siting).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
+      
     });
-
 });
-  
+
 
 var button = d3.select("#filter-btn");
 var form = d3.select("form");
@@ -30,4 +30,16 @@ function runEnter() {
   var inputValue = inputElement.property("value");
   console.log(inputValue);
   
+  var filteredData = tableData.filter(dates => dates.datetime === inputValue);
+  console.log(filteredData);
+  
+  filteredData.forEach((siting) => {
+    var row = tbody.append("tr");
+
+    Object.entries(siting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+
+    });
+  });
 };
